3786b01dbf73fd41683e3fbc9a861800a6ec247c
Thanh Hai Mai <thanhhaipmai@gmail.com>
2013-01-19 17:37:45 -0800

    + add carousel bootstrap template

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'thanh hai mai' });
+  // res.render('index', { title: 'thanh hai mai' });
+  res.render('carousel', { title: 'thanh hai mai' });
 };
